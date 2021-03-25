614167bfbf6bea2bbc1a125db2a17abf80f984d8
John Hiltenbrand <jhiltenb@gmail.com>
2021-03-24 21:15:35 -0500

    Refactor cube cells

--- js/cube.module.js
+++ js/cube.module.js
@@ -21,6 +21,60 @@ class Animatable {
     }
 }
 
+class CubeCell extends Group {
+    constructor(x=1, y=1, z=1, size=10) {
+        super();
+        this.size = size * 1.02;
+        const planeSize = size * 0.95;
+        const boxSize   = size * 0.995;
+
+        const baseMaterial    = new MeshStandardMaterial( { color: 0x202020, flatShading: true } );
+        const redMaterial     = new MeshStandardMaterial( { color: 0xff0000, flatShading: true } );
+        const greenMaterial   = new MeshStandardMaterial( { color: 0x00ff00, flatShading: true } );
+        const blueMaterial    = new MeshStandardMaterial( { color: 0x0000ff, flatShading: true } );
+        const orangeMaterial  = new MeshStandardMaterial( { color: 0xff8000, flatShading: true } );
+        const whiteMaterial   = new MeshStandardMaterial( { color: 0xffffff, flatShading: true } );
+        const yellowMaterial  = new MeshStandardMaterial( { color: 0xffff00, flatShading: true } );
+        const geometry = new PlaneGeometry( planeSize, planeSize, 1, 1);
+
+        const boxMesh = new Mesh( new BoxGeometry(boxSize, boxSize, boxSize), baseMaterial);
+        this.add(boxMesh);
+
+        const topMesh = new Mesh( geometry, whiteMaterial);
+        topMesh.position.y = size / 2;
+        topMesh.rotation.x = -Math.PI / 2;
+        this.add( topMesh );
+
+        const bottomMesh = new Mesh( geometry, yellowMaterial);
+        bottomMesh.position.y = size / -2;
+        bottomMesh.rotation.x = Math.PI / 2;
+        this.add( bottomMesh );
+
+        const frontMesh = new Mesh( geometry, greenMaterial);
+        frontMesh.position.z = size / 2;
+        this.add( frontMesh );
+
+        const backMesh = new Mesh( geometry, blueMaterial);
+        backMesh.position.z = size / -2;
+        backMesh.rotation.x = Math.PI;
+        this.add( backMesh );
+
+        const leftMesh = new Mesh( geometry, orangeMaterial);
+        leftMesh.position.x = size / -2;
+        leftMesh.rotation.y = -Math.PI / 2;
+        this.add( leftMesh );
+
+        const rightMesh = new Mesh( geometry, redMaterial);
+        rightMesh.position.x = size / 2;
+        rightMesh.rotation.y = Math.PI / 2;
+        this.add( rightMesh );
+        this.position.x = x * this.size;
+        this.position.y = y * this.size;
+        this.position.z = z * this.size;
+
+    }
+}
+
 export class Cube {
   constructor(depth=3) {
     this.depth = depth;
@@ -30,83 +84,26 @@ export class Cube {
 
   getMesh() {
     if( this.group == null ) {
-        const baseMaterial    = new MeshStandardMaterial( { color: 0x202020, flatShading: true } );
-        const redMaterial     = new MeshStandardMaterial( { color: 0xff0000, flatShading: true } );
-        const greenMaterial   = new MeshStandardMaterial( { color: 0x00ff00, flatShading: true } );
-        const blueMaterial    = new MeshStandardMaterial( { color: 0x0000ff, flatShading: true } );
-        const orangeMaterial  = new MeshStandardMaterial( { color: 0xff8000, flatShading: true } );
-        const whiteMaterial   = new MeshStandardMaterial( { color: 0xffffff, flatShading: true } );
-        const yellowMaterial  = new MeshStandardMaterial( { color: 0xffff00, flatShading: true } );
-        const geometry = new PlaneGeometry( 9.5, 9.5, 1, 1);
-
-        const cube = new Group();
-
-        const boxMesh = new Mesh( new BoxGeometry(9.95,9.95,9.95), baseMaterial);
-        cube.add(boxMesh);
-
-        const topMesh = new Mesh( geometry, whiteMaterial);
-        topMesh.position.y = 5;
-        topMesh.rotation.x = -Math.PI/2;
-        cube.add( topMesh );
-
-        const bottomMesh = new Mesh( geometry, yellowMaterial);
-        bottomMesh.position.y = -5;
-        bottomMesh.rotation.x = Math.PI/2;
-        cube.add( bottomMesh );
-
-        const frontMesh = new Mesh( geometry, greenMaterial);
-        frontMesh.position.z = 5;
-        cube.add( frontMesh );
-
-        const backMesh = new Mesh( geometry, blueMaterial);
-        backMesh.position.z = -5;
-        backMesh.rotation.x = Math.PI;
-        cube.add( backMesh );
-
-        const leftMesh = new Mesh( geometry, orangeMaterial);
-        leftMesh.position.x = -5;
-        leftMesh.rotation.y = -Math.PI / 2;
-        cube.add( leftMesh );
-
-        const rightMesh = new Mesh( geometry, redMaterial);
-        rightMesh.position.x = 5;
-        rightMesh.rotation.y = Math.PI / 2;
-        cube.add( rightMesh );
-
-        const col = new Group();
-        col.add( cube );
-
-        const cube2 = cube.clone(true);
-        cube2.position.z = 10.1;
-        col.add( cube2 );
-
-        const cube3 = cube.clone(true);
-        cube3.position.z = -10.1;
-        col.add( cube3 );
-
-        const slice = new Group();
-        slice.add( col );
-
-        const col2 = col.clone(true);
-        col2.position.y = 10.1;
-        slice.add( col2 );
-
-        const col3 = col.clone(true);
-        col3.position.y = -10.1;
-        slice.add( col3 );
-
-        this.group = new Group();
-        this.group.add( slice );
-
-        const slice2 = slice.clone(true);
-        slice2.position.x = 10.1;
-        this.group.add( slice2 );
-
-        const slice3 = slice.clone(true);
-        slice3.position.x = -10.1;
-        this.group.add( slice3 );
-        this.state = 0;
-        this.step = 0;
+      this.group = new Group();
+      this.state = 0;
+      this.step = 0;
+      this.data = [];
+      const mp = ((this.depth - 1) / 2);
+      for (var x = 0; x < this.depth; x++) {
+        if (typeof this.data[x] === 'undefined') {
+          this.data[x] = []
+        }
+        for (var y = 0; y < this.depth; y++) {
+          if (typeof this.data[x][y] === 'undefined') {
+            this.data[x][y] = []
+          }
+          for (var z = 0; z < this.depth; z++) {
+            const cell = new CubeCell(x - mp, y - mp, z - mp);
+            this.data[x][y][z] = cell;
+            this.group.add(cell);
+          }
+        }
+      }
     }
     return this.group;
   }
